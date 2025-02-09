755b8100c697db73bb8d188d7b1e9a9226fbecf6
pkazsully <youabsolutenerd@gmail.com>
2025-02-08 16:54:20 -0800

    Finished run initalize functions, added class for storing nodes

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
 #Needed for deep copying puzzles
 import copy
 
+class Node:
+    def __init__(self, puzzle, g, h):
+        self.puzzle = puzzle
+        self.g = g
+        self.h = h
+    
+    def __lt__(self, other):
+        return (self.g + self.h) < (other.g + other.h)
+    
+    def __eq__(self, other):
+        return self.puzzle == other.puzzle
+
 rows = 3
 cols = 3
 
+#solved puzzle
 goal_state = [[1,2,3],
               [4,5,6],
               [7,8,0]]
 
-test1 = [[1,2,3],
-         [5,0,6],
-         [4,7,8]]
+#puzzles given in the assignment handout
+given_puzzles = [[[1,2,3],
+                  [4,5,6],
+                  [0,7,8]],
 
+                 [[1,2,3],
+                  [5,0,6],
+                  [4,7,8]],
+
+                 [[1,3,6],
+                  [5,0,2],
+                  [4,7,8]],
+                  
+                 [[1,3,6],
+                  [5,0,7],
+                  [4,8,2]],
+                  
+                 [[1,6,7],
+                  [5,0,3],
+                  [4,8,2]],
+                  
+                 [[7,1,2],
+                  [4,8,5],
+                  [6,3,0]],
+                
+                 [[0,7,2],
+                  [4,6,1],
+                  [3,5,8]]]
+
+def get_uc_heuristic(puzzle):
+    return 0
 
 def get_mt_heuristic(puzzle):
     heuristic = 0
@@ -67,19 +107,46 @@ def make_moves(puzzle, moves):
 
 def search(puzzle, heuristic):
 
+#Initialize a run using one of the given puzzles
 def default_run():
+    print("Enter a number from 1-7 to choose one of Dr. Keogh's given puzzles")
+    puzzle = input()
+    print("Enter '1' to use uniform cost search, '2' for A* with misplaced tile, or '3' for A* with manhattan distance")
+    alg = input()
+    if(alg == 1):
+        search(given_puzzles[puzzle], get_uc_heuristic)
+    elif(alg == 2):
+        search(given_puzzles[puzzle], get_mt_heuristic)
+    elif(alg ==3):
+        search(given_puzzles[puzzle], get_md_heuristic)
 
+#Initialize a run using a user inputted puzzle. Should probably have input filtering/cleaning, but I'll only add if I have time
 def custom_run():
-
+    print("Enter your puzzle, row by row, with 0 representing the blank spot. Press enter once you have finished a row, and leave spaces in between numbers\n")
+    print("Row 1:")
+    row1 = input()
+    print("Row 2:")
+    row2 = input()
+    print("Row 3:")
+    row3 = input()
+    puzzle = [list(map(int, row1.split())), list(map(int, row2.split())), list(map(int, row3.split()))]
+    print("Enter '1' to use uniform cost search, '2' for A* with misplaced tile, or '3' for A* with manhattan distance")
+    alg = input()
+    if(alg == 1):
+        search(given_puzzles[puzzle], get_uc_heuristic)
+    elif(alg == 2):
+        search(given_puzzles[puzzle], get_mt_heuristic)
+    elif(alg ==3):
+        search(given_puzzles[puzzle], get_md_heuristic)
 
 
 
 def main():
-    print("Welcome to Peter Sullivan's CS170 Project 1. Enter '1' to use one of the puzzles provided in Professor Keogh's handout or '2' to enter your own:")
+    print("Welcome to Peter Sullivan's CS170 Project 1. Enter '1' to use one of the puzzles provided in Dr. Keogh's handout or '2' to enter your own:")
     mode = input()
     if mode == 1:
         default_run()
     elif mode == 2:
         custom_run()
-    else
+    return
 
